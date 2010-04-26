4d08e2e26c41267dc50023bdc3992b229dffce0f
Wilton de Oliveira Garcia <wiltondeoliveiragarcia@wilton.local>
2010-04-26 12:13:08 -0300

    Deletados arquivos temporarios

--- template/header.tmpt.php
+++ template/header.tmpt.php
@@ -21,16 +21,6 @@
 				catch(e){} 
 			}
 		</script>
-		<script type="text/javascript">
-			var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-			document.write(unescape("%3Cscript src=\'" + gaJsHost + "google-analytics.com/ga.js\' type=\'text/javascript\'%3E%3C/script%3E"));
-			</script>
-			<script type="text/javascript">
-			try {
-			var pageTracker = _gat._getTracker("UA-16049399-1");
-			pageTracker._trackPageview();
-			} catch(err) {}
-		</script>
 		 <style>
         	<!--
 			@import url("library/css/estilo.css");
